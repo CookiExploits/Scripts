e5f79739fadf6e96773dcfeadaf24a007b33ed6a
CookiExploits <89049862+CookiExploits@users.noreply.github.com>
2021-10-07 07:42:05 -0700

    Updated for easier gui making
    idk

--- template.lua
+++ template.lua
@@ -1,11 +1,11 @@
 local p = game.Players.LocalPlayer
-local RS = game:GetService("ReplicatedStorage")
+local rs = game:GetService("ReplicatedStorage")
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/BadScripter/Scripts/master/WallyGUILibV2.lua", true))()
-local Auto = library:CreateWindow("Template")
-local Misc = library:CreateWindow("Misc | Settings")
+local w = library:CreateWindow("Template")
+local a = library:CreateWindow("Misc | Settings")
 
 
-Misc:Box('WalkSpeed', {
+a:Box('WalkSpeed', {
    flag = "ws";
    type = 'number';
 }, function(new, old, enter)
@@ -13,7 +13,7 @@ Misc:Box('WalkSpeed', {
    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = tonumber(new)
 end)
 
-Misc:Box('JumpPower', {
+a:Box('JumpPower', {
    flag = "jp";
    type = 'number';
 }, function(new, old, enter)
@@ -21,10 +21,10 @@ Misc:Box('JumpPower', {
    game.Players.LocalPlayer.Character.Humanoid.JumpPower = tonumber(new)
 end)
 
-Misc:Button('Server Hop', function()
+a:Button('Server Hop', function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/CookiExploits/Misc-Scripts/master/SH.lua"))()
 end)
 
-Misc:Button('DestroyGui', function()
+a:Button('DestroyGui', function()
     game.CoreGui.ScreenGui:Destroy()
 end)
